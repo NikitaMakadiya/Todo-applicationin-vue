d217ed51027c7062cd1515af2e71a4aec656b7d8
NikitaMakadiya <nikitamakadiya128@gmail.com>
2025-11-29 16:06:53 +0530

    create project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- <home-com /> -->
+    <TodoList />
+  </div>
+</template>
+
+<script>
+// import HomeCom from "./components/Home.vue";
+import TodoList from "./components/Todo.vue";
+export default {
+  name: "App",
+  components: { TodoList },
+};
+</script>
+
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>function call</h1>
+
+    <h2>{{ name }}</h2>
+
+    <button @click="changename">change name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeCom",
+
+  data() {
+    return {
+      name: "My name is nikita",
+    };
+  },
+
+  methods: {
+    changename() {
+      this.name = "My name is nikky";
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+}
+</style>
